03bb75142d8f88cfd9a587816e34a6e45bcd8056
Robin Perälä <robin.perala@student.hanken.fi>
2021-10-07 23:19:43 +0300

    perala

--- EMF_2021_Perälä_Krusberg.R
+++ EMF_2021_Perälä_Krusberg.R
@@ -3,7 +3,7 @@
 #Set your working directory to source file location!
 #Or launch R in the project folder
 
-setwd("c://emf_2021//EMF_2021_Perälä_Krusberg")
+setwd("c://emf_2021//EMF_2021_Perala_Krusberg")
 
 rm(list=ls());
 cat("\014");

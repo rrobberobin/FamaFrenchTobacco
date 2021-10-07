cb26885afc250c6372a29f6dc76b0f64fdde24d5
Robin Perälä <robin.perala@student.hanken.fi>
2021-10-07 21:48:10 +0300

    Names2

--- EMF_2021_Perälä_Krusberg.R
+++ EMF_2021_Perälä_Krusberg.R
@@ -5,7 +5,6 @@
 
 rm(list=ls());
 cat("\014");
-
 library(readxl)
 
 
@@ -26,12 +25,8 @@ Fama = Fama[,-1]  #remove the dates
 Fama = as.matrix(Fama)  #make dataframe into matrix 
 
 excessReturns = smoke - Fama[,6]
-
-
 Fama = Fama[,1:5] #Remove the riskfree rate column
 
-#använd i ställe:
-#lm(excessReturns ~., data=as.data.frame(Fama))
 reg1 = lm(excessReturns ~ Fama)
 names(reg1$coefficients) <- c("(Intercept)", colnames(Fama))
 summary(reg1)
@@ -89,7 +84,7 @@ hist(residuals(reg1), breaks=50, main = "Histogram of model residuals")
 #If we would have daily data, we would get a bigger sample, and the static would become more normally distributed (CLT would apply)
 
 #Let's try make a logarithmic transform of the returns
-logRet = lm(log(1+excessReturns/100) ~ log(1+Fama/100))
+logRet = lm(excessReturns ~ log(1+Fama/100))
 summary(logRet)
 hist(residuals(logRet), breaks=50)
 jarque.test(residuals(logRet)) #no improvement. Now the residuals are even less normal
@@ -97,8 +92,7 @@ jarque.test(residuals(logRet)) #no improvement. Now the residuals are even less 
 #We expect heteroskedasticity:
 #Heteroskedasticity test: White
 white = lm(residuals(reg1)^2 ~.^2, data=as.data.frame(Fama))
-whiteDesc = summary(white)
-NR2 = nobs(reg1) * whiteDesc$r.squared
+NR2 = nobs(reg1) * summary(white)$r.squared
 pValue = 1 - pchisq(NR2,ncol(model.matrix(white)[,-1])) 
 pValue #p-value is approximately 0.45. We reject the null hypothesis on significance values of 1, 5 and 10 percent. Seems like we have heteroskedasticity
 
@@ -134,12 +128,16 @@ linearHypothesis(reg1, nullhyp4, vcov=vcovHAC(reg1)) #model is still significant
 
 
 #Information criteria
-round(AIC(reg1),2)
-round(BIC(reg1),2)
+round(AIC(reg1, reg2, logRet),2)
+round(BIC(reg1, reg2, logRet),2)
+
+#adjusted R squared
+round(c(summary(reg1)$adj.r.squared, summary(reg2)$adj.r.squared, summary(logRet)$adj.r.squared), 4)
+
 
 
 library(stargazer)
-stargazer(reg1,
+stargazer(reg1, reg2,
           type="text")
 
 
